0dcf46f481010a18e218bd25cfda8e5a77d60bc2
Tim Mui <timmui123@gmail.com>
2016-11-26 13:00:04 -0500

    Add reset endpoint

--- routes.js
+++ routes.js
@@ -5,9 +5,9 @@ var io;
 module.exports = function (ioIn) {
     io = ioIn;
     // Configure Endpoints
-    router.get('/clear', (req, res) => {
-        io.emit('clear');
-        res.send({});
+    router.get('/reset', (req, res) => {
+        io.emit('reset');
+        res.send({ message: 'Sent Reset' });
     });
 
     return router;
