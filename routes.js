d90d54d22ef06bb4998482c037cf91b0c6283b75
Tim Mui <timmui123@gmail.com>
2016-11-26 14:56:40 -0500

    Add fix for parsing floats

--- routes.js
+++ routes.js
@@ -9,9 +9,11 @@ var intensityThrottle = 1000;
 var intensityQueue = [];
 var throttledIntensity = _.throttle(function emitRotation() {
     var value = _.mean(intensityQueue);
+    console.log(`Sending intensity: ${value}`);
     io.emit('intensity', {
         amount: value,
     });
+    intensityQueue = [];
 }, intensityThrottle);
 
 // Rotation
@@ -19,9 +21,11 @@ var rotationThrottle = 1000;
 var rotationQueue = [];
 var throttledRotation = _.throttle(function emitRotation() {
     var value = _.mean(rotationQueue);
+    console.log(`Sending rotation: ${value}`);
     io.emit('rotation', {
         amount: value,
     });
+    rotationQueue = [];
 }, rotationThrottle);
 
 // deltaEyes
@@ -29,9 +33,11 @@ var deltaEyesThrottle = 1000;
 var deltaEyesQueue = [];
 var throttledDeltaEyes = _.throttle(function emitDeltaEyes() {
     var value = _.mean(deltaEyesQueue);
+    console.log(`Sending deltaEyes: ${value}`);
     io.emit('deltaEyes', {
         amount: value,
     });
+    deltaEyesQueue = [];
 }, deltaEyesThrottle);
 
 module.exports = function (ioIn) {
@@ -43,7 +49,7 @@ module.exports = function (ioIn) {
     });
 
     router.get('/intensity', (req, res) => {
-        intensityQueue.push(req.query.amount);
+        intensityQueue.push(parseFloat(req.query.amount));
 
         throttledIntensity();
 
@@ -51,7 +57,7 @@ module.exports = function (ioIn) {
     });
 
     router.get('/rotation', (req, res) => {
-        rotationQueue.push(req.query.amount);
+        rotationQueue.push(parseFloat(req.query.amount));
 
         throttledRotation();
 
@@ -59,7 +65,7 @@ module.exports = function (ioIn) {
     });
 
     router.get('/deltaEyes', (req, res) => {
-        deltaEyesQueue.push(req.query.amount);
+        deltaEyesQueue.push(parseFloat(req.query.amount));
 
         throttledDeltaEyes();
 
